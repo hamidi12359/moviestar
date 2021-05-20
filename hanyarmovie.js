f1b300b815c609de6c0bc7880b0b0858f29eec3b
hamidi12359 <84512667+hamidi12359@users.noreply.github.com>
2021-05-20 21:19:22 +0800

    Create hanyarmovie.js

---
+++ hanyarmovie.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+  
+  'https://www.achievablecpmrevenue.com/fev6zvvj?key=09d5b2b55d8e386c4039b7d2e8383f24'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
