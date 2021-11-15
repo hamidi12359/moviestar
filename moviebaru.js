38fb3c36099aec15548f3343fbca156f2f775bc6
hamidi12359 <84512667+hamidi12359@users.noreply.github.com>
2021-11-15 10:26:10 +0800

    Update moviebaru.js

--- moviebaru.js
+++ moviebaru.js
@@ -1,5 +1,6 @@
 var dataUrl = [
-  'https://declinedmildlyreckon.com/y9nwnsv2?key=735cd9e19225596576e6fe8a6476073f'
+  'https://declinedmildlyreckon.com/y9nwnsv2?key=735cd9e19225596576e6fe8a6476073f',
+  'https://declinedmildlyreckon.com/b22m2ndta?key=eb344a22403a965c7b203e418b50b181'
   
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
